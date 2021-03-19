2a9564b2ad58c6f28b498622a35ee92a580132c5
Pshaw397 <phillip.a.shaw@googlemail.com>
2021-03-19 17:39:07 +0000

    Added radio project in progress

---
+++ Radio_Mini_Project/RadioApp/Radio.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Media;
+namespace RadioApp
+{
+    public class Radio
+    {
+        private int _channel = 1;
+        private bool _on = false;
+
+        public int Channel 
+        {
+            get { return _channel; }
+            set 
+            {
+                if (_on == true & value > 0 & value < 5)
+                {
+                    _channel = value;
+                }
+                else if (_on == false)
+                {
+                    _channel = 1;
+                }
+            } 
+        }
+
+        public bool On { get { return _on; } }
+
+        public string Play()
+        {
+            if (_on == true)
+            {
+                return $"Playing channel {Channel}";
+
+                switch (Channel)
+                {
+                    case 1:
+                        {
+                            
+                            break;
+                            
+                        }
+                }
+            }
+            else
+            {
+                return "Radio is off";
+            }
+        }
+
+        public void TurnOff()
+        {
+            _on = false;
+        }
+
+        public void TurnOn()
+        {
+            _channel = 1;
+            _on = true;
+        }
+    }
+    // implement a class Radio that corresponds to the Class diagram 
+    //   and specification in the Radio_Mini_Project document
+}
---
+++ Radio_Mini_Project/RadioWPF/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RadioApp;
+using System.Diagnostics;
+
+namespace RadioWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Radio radioPlayer = new Radio();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mediaElementCh1.Source = new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p", UriKind.RelativeOrAbsolute); // BBC Radio 1
+            mediaElementCh2.Source = new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p", UriKind.RelativeOrAbsolute); // BBC Radio 2
+            mediaElementCh3.Source = new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p", UriKind.RelativeOrAbsolute); // BBC Radio 3
+            mediaElementCh4.Source = new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4_mf_p", UriKind.RelativeOrAbsolute); // BBC Radio 4
+
+        }
+
+        public class RadioControl
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedBtn = (Button)sender;
+            if (clickedBtn.Content.ToString() == "Channel 1")
+            {
+                radioPlayer.Channel = 1;
+            }
+            if (clickedBtn.Content.ToString() == "Play" & radioPlayer.On == true)
+            {
+                switch (radioPlayer.Channel)
+                {
+                    case 1:
+                        {
+                            mediaElementCh1.Play();
+                            break;
+                        }
+                        // I NEED TO INPUT THE CASES FOR RADIO'S 2-4 AND PUT THE CHANNEL BUTTONS IN
+                }
+            }
+
+        }
+
+        private void radioOn_Checked(object sender, RoutedEventArgs e)
+        {
+            if (radioOn.IsChecked == true)
+            {
+                radioPlayer.TurnOn();
+            }
+        }
+
+        private void radioOff_Checked(object sender, RoutedEventArgs e)
+        {
+            if (radioOff.IsChecked == true)
+            {
+                radioPlayer.TurnOff();
+            }
+        }
+    }
+}
